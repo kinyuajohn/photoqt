a314fdec4545e68ba197da227fd341f9919526d6
John Kinya <kingjohnkn@gmail.com>
2024-05-21 17:42:21 +0300

    add PIL functionality

--- photo_qt.py
+++ photo_qt.py
@@ -131,6 +131,54 @@ class Editor:
         picture_box.setPixmap(image)
         picture_box.show()
 
+    def gray(self):
+        self.image = self.image.convert("L")
+        self.save_image()
+        image_path = os.path.join(working_directory, self.save_folder, self.filename)
+        self.show_image(image_path)
+
+    def left(self):
+        self.image = self.image.transpose(Image.ROTATE_90)
+        self.save_image()
+        image_path = os.path.join(working_directory, self.save_folder, self.filename)
+        self.show_image(image_path)
+
+    def right(self):
+        self.image = self.image.transpose(Image.ROTATE_270)
+        self.save_image()
+        image_path = os.path.join(working_directory, self.save_folder, self.filename)
+        self.show_image(image_path)
+
+    def mirror(self):
+        self.image = self.image.transpose(Image.FLIP_LEFT_RIGHT)
+        self.save_image()
+        image_path = os.path.join(working_directory, self.save_folder, self.filename)
+        self.show_image(image_path)
+
+    def sharpen(self):
+        self.image = self.image.filter(ImageFilter.SHARPEN)
+        self.save_image()
+        image_path = os.path.join(working_directory, self.save_folder, self.filename)
+        self.show_image(image_path)
+
+    def blur(self):
+        self.image = self.image.filter(ImageFilter.BLUR)
+        self.save_image()
+        image_path = os.path.join(working_directory, self.save_folder, self.filename)
+        self.show_image(image_path)
+
+    def color(self):
+        self.image = ImageEnhance.Color(self.image).enhance(1.2)
+        self.save_image()
+        image_path = os.path.join(working_directory, self.save_folder, self.filename)
+        self.show_image(image_path)
+
+    def contrast(self):
+        self.image = ImageEnhance.Contrast(self.image).enhance(1.2)
+        self.save_image()
+        image_path = os.path.join(working_directory, self.save_folder, self.filename)
+        self.show_image(image_path)
+
 
 def displayImage():
     if file_list.currentRow() >= 0:
@@ -143,6 +191,16 @@ main = Editor()
 
 btn_folder.clicked.connect(getWorkDirectory)
 file_list.currentRowChanged.connect(displayImage)
+
+gray.clicked.connect(main.gray)
+btn_left.clicked.connect(main.left)
+btn_right.clicked.connect(main.right)
+sharpness.clicked.connect(main.sharpen)
+saturation.clicked.connect(main.color)
+contrast.clicked.connect(main.contrast)
+blur.clicked.connect(main.blur)
+mirror.clicked.connect(main.mirror)
+
 
 # Run/Show
 main_window.show()
